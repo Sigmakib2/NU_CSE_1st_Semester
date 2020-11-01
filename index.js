eca0a12371191456cec1c35233f7f5df4381b694
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2020-11-01 20:00:27 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+// Modal Image Gallery
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
+
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+    } else {
+        mySidebar.style.display = 'block';
+    }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+//my work sarted
+function syllabus() {
+    location.href = "syllabus.html";
+}
+
+function spl() {
+    location.href = "splcontent.html";
+}
+
+function ece() {
+    location.href = "ece.html";
+}
+
+function cal() {
+    location.href = "cal.html";
+}
+function cal2() {
+    location.href = "cal2.html";
+}
+
+function phy() {
+    location.href = "phy.html";
+}
+function qb() {
+    location.href = "qb.html";
+}
+function qb2() {
+    location.href = "qb2.html";
+}
+function app() {
+    location.href = "https://drive.google.com/file/d/11kB96ID0WPRXEGvlgjCDezggbc6_EGva/view?usp=sharing";
+}
